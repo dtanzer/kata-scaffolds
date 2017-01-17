8df50b515c849d3abcf91f079fd16e6b34781cbc
David Tanzer <business@davidtanzer.net>
2017-01-17 16:02:06 +0100

    Initial tests

--- hangman/src/main/java/net/davidtanzer/katas/hangman/Hangman.java
+++ hangman/src/main/java/net/davidtanzer/katas/hangman/Hangman.java
@@ -1,4 +1,34 @@
 package net.davidtanzer.katas.hangman;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Hangman {
+	
+	private int wrongGuesses;
+	private String word ;
+	
+	private Hangman(final String word) {
+		this.word = word;
+	}
+	
+	public char[] getStatus() {
+		char [] x = {'_','_','_','_','_'} ;
+		return x;
+	}
+	
+	public static Hangman start(final String word) {
+		return new Hangman(word) ;
+	}
+	
+	public int getWrongGuesses() {
+		return wrongGuesses;
+	}
+	
+	public void guess(final char letter) {
+		boolean letterIsInWord = word.indexOf(letter) > -1;
+		if (!letterIsInWord){
+			wrongGuesses++;
+		}
+	}
 }
--- hangman/src/test/java/net/davidtanzer/katas/hangman/HangmanTest.java
+++ hangman/src/test/java/net/davidtanzer/katas/hangman/HangmanTest.java
@@ -1,20 +1,51 @@
 package net.davidtanzer.katas.hangman;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class HangmanTest {
+	
 	private Hangman hangman;
 	
-	@Before
-	public void setup() {
-		hangman = new Hangman();
+	@Test
+	public void shouldAnUnderscoreForEveryLetterInWordOnStart() {
+		String word = "test1";
+		
+		hangman = Hangman.start(word);
+		
+		char[] status = hangman.getStatus();
+		char[] expecteds = word.replaceAll(".", "_").toCharArray();
+		assertArrayEquals(expecteds, status);
 	}
 	
 	@Test
-	public void failingTestStub() {
-		fail("Implement me!");
+	public void shouldIncrementWrongGuessesOnWrongLetter() {
+		String word = "a";
+		char notInWord = 'b';
+		
+		hangman = Hangman.start(word);
+		int wrongGuesses = hangman.getWrongGuesses();
+		
+		hangman.guess(notInWord);
+		
+		assertEquals("wrongGuesses", wrongGuesses + 1, hangman.getWrongGuesses());
+		
+		
+	}
+	
+	@Test
+	public void shouldNotIncrementWrongGuessesOnRightLetter() {
+		String word = "a";
+		char inWord = 'a';
+		
+		hangman = Hangman.start(word);
+		int wrongGuesses = hangman.getWrongGuesses();
+		
+		hangman.guess(inWord);
+		
+		assertEquals("wrongGuesses", wrongGuesses,
+				hangman.getWrongGuesses());
 	}
 }
